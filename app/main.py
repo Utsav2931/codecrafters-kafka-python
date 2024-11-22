8213bc0c7084050239a0dc81dac4f95a0bcd6e77
Utsav <upatel231a@gmail.com>
2024-11-21 20:45:45 -0800

    Sending response to client.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,16 @@
-import socket  # noqa: F401
+import socket
+
+def handle_client(client : socket.socket):
+    # Receives client request 
+    req = client.recv(1024)
+    print("Request from client",req)
+
+    # Send response with size and corelation_id
+    size = 0
+    corelation_id = 7
+    client.send(size.to_bytes(4, byteorder = "big", signed = True))
+    client.send(corelation_id.to_bytes(4, byteorder = "big", signed = True))
+    client.close()
 
 
 def main():
@@ -8,7 +20,10 @@ def main():
 
     # Uncomment this to pass the first stage
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    server.accept() # wait for client
+    while True:
+        client, _ = server.accept() # wait for client
+        handle_client(client)
+
 
 
 if __name__ == "__main__":
